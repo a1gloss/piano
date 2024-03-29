ac1b580b05f0effbeedfa6fdf05ffb3442b9b4a8
amanda <amanda.caztro@gmail.com>
2024-03-29 08:33:08 -0300

    :D

---
+++ script.js
@@ -0,0 +1,13 @@
+const keys = document.querySelectorAll('.key')
+
+const handleClick = (key) => {
+
+    key.style.background = '#ddd'
+
+}
+
+keys.forEach((key)=> {
+
+    key.addEventListener('click', () => handleClick (key))
+
+});
